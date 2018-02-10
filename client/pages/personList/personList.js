8d0a1e21f47360f5fec41d0aca22dfba531c008c
leo-chaoliu <leo@themrolab-singpaore.com>
2018-02-10 10:22:32 +0800

    navbar for person list

--- client/pages/personList/personList.js
+++ client/pages/personList/personList.js
@@ -3,26 +3,36 @@ var config = require('../../config');
 var util = require('../../utils/util.js');
 var that;
 
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
   data: {
     personList: [],
+    personListBoy: [],
+    personListGirl: [],
     personDtl: [],
     dateNow: 0,
-    hometown: ''
+
+    //navbar
+    tabs: ["男生", "女生"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
 
   onLoad: function (options) {
     that = this;
-    //load all the person information and list down 
-    // getAllPerson();
-    // getByIDPerson(102)
-    
+    that.initialData();
+    that.setTabData();
   },
 
   onShow: function () {
     getAllPerson();
     // getByIDPerson(102)
+  },
 
+  initialData() {
+    //calculate this getFullYear
     var d = new Date();
     var n = d.getFullYear() - 1 - 1993;
     console.log(n)
@@ -31,40 +41,71 @@ Page({
     })
   },
 
-  openPerson: function(e){
-    console.log('open person clikced',e)
-    let dataPersonDtl = that.data.personList[e.currentTarget.id];
-    console.log('dataPersonDtl', dataPersonDtl)
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+    console.log(that.data.activeIndex)
+  },
+
+  setTabData: function () {
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+
+    //todo 
+    //get user info if gender == "nan" => activeIndex == 0 else => ==1
+  },
+
+  openPerson: function (e) {
+    console.log('open person clikced', e)
+    let dataTransfer;
+    if (that.data.activeIndex == 0) {
+      dataTransfer = that.data.personListBoy[e.currentTarget.id]
+    }
+    else if (that.data.activeIndex == 1) {
+      dataTransfer = that.data.personListGirl[e.currentTarget.id]
+    }
+    console.log('dataTransfer', dataTransfer)
     wx.navigateTo({
-      url: '../personDtl/personDtl?dataPersonDtl=' +  JSON.stringify(dataPersonDtl)
+      url: '../personDtl/personDtl?dataPersonDtl=' + JSON.stringify(dataTransfer)
     })
   }
-
-
 })
 
-function getAllPerson(){
+function getAllPerson() {
   // util.showBusy('');
   wx.request({
     url: config.service.getAllPerson,
     method: 'get',
-    success(result){
-      console.log('get personlist',result.data);
+    success(result) {
+      console.log('get personlist', result.data);
       that.setData({
-        personList : result.data.personInfo
+        // personList: result.data.personInfo,
+        personListBoy: result.data.personInfo.filter(x => x.gender == "男"),
+        personListGirl: result.data.personInfo.filter(x => x.gender == "女")
       });
-      console.log('that.personList',that.data.personList);
+      // console.log('that.personList', that.data.personList);
+      console.log('that.personListBoy', that.data.personListBoy);
+      console.log('that.personListGirl', that.data.personListGirl);
+
       // util.showSuccess('');
     },
-    fail(error){
+    fail(error) {
       // util.showModel('请求失败',error);
-      console.error('request fail',error);
+      console.error('request fail', error);
     }
   });
 }
 
 function getByIDPerson(id) {
-  
+
   // util.showBusy('');
   wx.request({
     url: config.service.getByIDPerson + id,
